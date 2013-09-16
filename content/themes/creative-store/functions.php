bd1f12e3fd8a17870bb666c72da0a9263fdb1b05
Michael Bragg <michael@michaelbragg.net>
2013-09-16 16:06:59 +0100

    Adds class to control the size of text in sidebar

--- content/themes/creative-store/functions.php
+++ content/themes/creative-store/functions.php
@@ -73,7 +73,7 @@ function creative_store_widgets_init() {
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
+		'before_title'  => '<h1 class="gamma widget-title">',
 		'after_title'   => '</h1>',
 	) );
 }
--- content/themes/creative-store/sidebar.php
+++ content/themes/creative-store/sidebar.php
@@ -14,14 +14,14 @@
 			</aside>
 
 			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'creative-store' ); ?></h1>
+				<h1 class="gamma widget-title"><?php _e( 'Archives', 'creative-store' ); ?></h1>
 				<ul>
 					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 				</ul>
 			</aside>
 
 			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'creative-store' ); ?></h1>
+				<h1 class="gamma widget-title"><?php _e( 'Meta', 'creative-store' ); ?></h1>
 				<ul>
 					<?php wp_register(); ?>
 					<li><?php wp_loginout(); ?></li>
