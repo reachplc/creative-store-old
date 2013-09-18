d9c007ee8292ee6ff7ce1d946bc47942698780ec
Michael Bragg <michael@michaelbragg.net>
2013-09-18 14:47:31 +0100

    Adds instruction bar for WooCommerce views

--- content/themes/creative-store/woocommerce/shop/wrapper-start.php
+++ content/themes/creative-store/woocommerce/shop/wrapper-start.php
@@ -10,3 +10,84 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 echo '<main id="main" class="site-main site-shop" role="main">';
+
+// Check to apply the correct process breadcrumbs state
+// depending on the page viewed
+if ( is_shop() ) {
+?>
+
+  <div class="instructions wrapper">
+    <ol class="list breadcrumbs breadcrumbs__instructions">
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_01.png" alt="">
+          <figcaption><em>1.</em> Select your area</figcation>
+        </figure>
+      </li>
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_02.png" alt="">
+          <figcaption><em>2.</em> Choose your design</figcation>
+        </figure>
+      </li>
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_03.png" alt="">
+          <figcaption><em>3.</em> Request and reserve</figcation>
+        </figure>
+      </li>
+    </ol>
+  </div><!--/ breadcrumbs breadcrumbs__instructions  -->
+
+<?php
+} elseif( is_product_category() ) {
+?>
+  <div class="instructions wrapper">
+    <ol class="list breadcrumbs breadcrumbs__instructions">
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_01.png" alt="">
+          <figcaption><em>1.</em> Select your area</figcation>
+        </figure>
+      </li>
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_02.png" alt="">
+          <figcaption><em>2.</em> Choose your design</figcation>
+        </figure>
+      </li>
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_03.png" alt="">
+          <figcaption><em>3.</em> Request and reserve</figcation>
+        </figure>
+      </li>
+    </ol>
+  </div><!--/ breadcrumbs breadcrumbs__instructions  -->
+<?php
+} elseif( is_product() ) {
+  ?>
+  <div class="instructions wrapper">
+    <ol class="list breadcrumbs breadcrumbs__instructions">
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_01.png" alt="">
+          <figcaption><em>1.</em> Select your area</figcation>
+        </figure>
+      </li>
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_02.png" alt="">
+          <figcaption><em>2.</em> Choose your design</figcation>
+        </figure>
+      </li>
+      <li class="text-center">
+        <figure>
+          <img src="<?php echo bloginfo('stylesheet_directory'); ?>/gui/breadcrumb-instructions_03.png" alt="">
+          <figcaption><em>3.</em> Request and reserve</figcation>
+        </figure>
+      </li>
+    </ol>
+  </div><!--/ breadcrumbs breadcrumbs__instructions  -->
+<?php
+}
