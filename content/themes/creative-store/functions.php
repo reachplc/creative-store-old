199d1f17a43e67b8d62d0f79ce10096f24d7c193
Michael Bragg <michael@michaelbragg.net>
2013-09-18 11:29:14 +0100

    Extracts WooCommerce wrapper from functions.php

--- content/themes/creative-store/functions.php
+++ content/themes/creative-store/functions.php
@@ -112,20 +112,6 @@ add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
  * WooCommerce scripts and styles
  */
 
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
-
-function my_theme_wrapper_start() {
-  echo '<main id="main" class="site-main site-shop" role="main">';
-}
-
-function my_theme_wrapper_end() {
-  echo '</main>';
-}
-
 // Declare WooCommerce support
 add_theme_support( 'woocommerce' );
 
