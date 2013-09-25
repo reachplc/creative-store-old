d94715eb7d27e23cfeddc905f243262215aaafe8
Michael Bragg <michael@michaelbragg.net>
2013-09-25 12:23:56 +0100

    Adds styling for single product view

--- content/themes/creative-store/woocommerce/content-single-product.php
+++ content/themes/creative-store/woocommerce/content-single-product.php
@@ -33,33 +33,35 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
     do_action( 'woocommerce_before_single_product_summary' );
   ?>
 
-  <div class="summary entry-summary">
+  <div class="products__view__info">
+
+    <div class="summary entry-summary">
+
+      <?php
+        /**
+         * woocommerce_single_product_summary hook
+         *
+         * @hooked woocommerce_template_single_title - 5
+         * @hooked woocommerce_template_single_price - 10
+         * @hooked woocommerce_template_single_excerpt - 20
+         * @hooked woocommerce_template_single_add_to_cart - 30
+         * @hooked woocommerce_template_single_meta - 40
+         * @hooked woocommerce_template_single_sharing - 50
+         */
+        do_action( 'woocommerce_single_product_summary' );
+      ?>
+
+    </div><!-- .summary -->
 
     <?php
       /**
-       * woocommerce_single_product_summary hook
+       * woocommerce_after_single_product_summary hook
        *
-       * @hooked woocommerce_template_single_title - 5
-       * @hooked woocommerce_template_single_price - 10
-       * @hooked woocommerce_template_single_excerpt - 20
-       * @hooked woocommerce_template_single_add_to_cart - 30
-       * @hooked woocommerce_template_single_meta - 40
-       * @hooked woocommerce_template_single_sharing - 50
+       * @hooked woocommerce_output_product_data_tabs - 10
+       * @hooked woocommerce_output_related_products - 20
        */
-      do_action( 'woocommerce_single_product_summary' );
+      do_action( 'woocommerce_after_single_product_summary' );
     ?>
-
-  </div><!-- .summary -->
-
-  <?php
-    /**
-     * woocommerce_after_single_product_summary hook
-     *
-     * @hooked woocommerce_output_product_data_tabs - 10
-     * @hooked woocommerce_output_related_products - 20
-     */
-    do_action( 'woocommerce_after_single_product_summary' );
-  ?>
 
 </div><!-- #product-<?php the_ID(); ?> -->
 
