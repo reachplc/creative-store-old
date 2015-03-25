9d5e4b46b90598252dbc24fd9dd018c375c3e3c1
Michael Bragg <michael@michaelbragg.net>
2015-03-25 12:28:52 +0000

    Add WP-API plugin

---
+++ web/content/mu-plugins/wp-api.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WP REST API
+ * Description: JSON-based REST API for WordPress, developed as part of GSoC 2013.
+ * Author: WP REST API Team
+ * Author URI: http://wp-api.org
+ * Version: 1.2.0
+ * Plugin URI: https://github.com/WP-API/WP-API
+ * License: GPL2+
+ */
+require WPMU_PLUGIN_DIR.'/WP-API/plugin.php';
