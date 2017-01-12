5dbab3ec5e557182b9a905f4aa8db7ff373f6ea4
Michael Bragg <michael@michaelbragg.net>
2017-01-12 10:47:50 +0000

    Add redirect for login fail

--- html/app/plugins-mu/tm-accounts.php
+++ html/app/plugins-mu/tm-accounts.php
@@ -54,3 +54,24 @@ add_filter(
 	'custom_login_page',
 	10, 3
 );
+
+/**
+ * Redirect failed login to referrer page
+ */
+
+function my_front_end_login_fail( $username ) {
+	/** Check if referer is present. */
+	if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
+		$referrer = esc_url( $_SERVER['HTTP_REFERER'] );
+	}
+	/** If there's a valid referrer, and it's not the default log-in screen. */
+	if ( ! empty( $referrer ) && ! strstr( $referrer,'wp-login' ) && ! strstr( $referrer,'wp-admin' ) ) {
+		wp_redirect( esc_url_raw( add_query_arg( array( 'status' => 'failed' ), $referrer ) ) );
+		exit;
+	}
+}
+
+add_action(
+	'wp_login_failed',
+	'my_front_end_login_fail'
+);
