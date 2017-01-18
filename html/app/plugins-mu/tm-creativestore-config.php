324ac687d09bf75102328cf9e71a704a7f4b5bfa
Michael Bragg <michael@michaelbragg.net>
2017-01-18 13:56:10 +0000

    Disable emojicons

--- html/app/plugins-mu/tm-creativestore-config.php
+++ html/app/plugins-mu/tm-creativestore-config.php
@@ -85,3 +85,58 @@ add_filter(
 	'ti_admin_footer',
 	'tm_creativestore_admin_footer'
 );
+
+/**
+ * Disable Emoji Icons
+ */
+function disable_wp_emojicons() {
+
+	/** All actions related to emojis. */
+	remove_action(
+		'admin_print_styles',
+		'print_emoji_styles'
+	);
+
+	remove_action(
+		'wp_head',
+		'print_emoji_detection_script',
+		7
+	);
+
+	remove_action(
+		'admin_print_scripts',
+		'print_emoji_detection_script'
+	);
+
+	remove_action(
+		'wp_print_styles',
+		'print_emoji_styles' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+	/** Filter to remove TinyMCE emojis. */
+	add_filter(
+		'tiny_mce_plugins',
+		'disable_emojicons_tinymce'
+	);
+}
+
+/**
+ * Disable Emojicons in Tinymce
+ *
+ * @param		array $plugins    Plugin list.
+ *
+ * @return	array						Empty array.
+ */
+function disable_emojicons_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	}
+	return array();
+}
+
+add_action(
+	'init',
+	'disable_wp_emojicons'
+);
