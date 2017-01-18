213b427a8ec26711d471d1083eb81921f6e1f451
Michael Bragg <michael@michaelbragg.net>
2017-01-18 13:39:41 +0000

    Add Admin Helper plugin
    Include overwrite for footer text.

--- html/app/plugins-mu/tm-creativestore-config.php
+++ html/app/plugins-mu/tm-creativestore-config.php
@@ -33,11 +33,12 @@ function wds_required_plugins_add( $required ) {
 		'brute-force-login-protection/brute-force-login-protection.php',
 		'buddypress/bp-loader.php',
 		'cmb2/init.php',
-    'safe-redirect-manager/safe-redirect-manager.php',
-    'stream/stream.php',
+		'safe-redirect-manager/safe-redirect-manager.php',
+		'stream/stream.php',
+		'ti-admin.php',
 		'woocommerce/woocommerce.php',
-    'wp-mail-smtp/wp_mail_smtp.php',
-    'wp-robots-txt/robots-txt.php',
+		'wp-mail-smtp/wp_mail_smtp.php',
+		'wp-robots-txt/robots-txt.php',
 	) );
 	return $required;
 }
@@ -63,4 +64,24 @@ function change_wds_required_plugins_text( $label ) {
 add_filter(
 	'wds_required_plugins_text',
 	'change_wds_required_plugins_text'
+);
+
+/**
+ * Modify object types used in TI Subtitle plugin.
+ *
+ * @return array	$object_types	New object types to use.
+ */
+function tm_creativestore_admin_footer() {
+
+	return sprintf(
+		'%1$s %2$s.',
+		esc_html( 'Created by', 'tm-admin' ),
+		esc_html( 'Trinity Mirror Creative', 'tm-admin' )
+	);
+
+}
+
+add_filter(
+	'ti_admin_footer',
+	'tm_creativestore_admin_footer'
 );
