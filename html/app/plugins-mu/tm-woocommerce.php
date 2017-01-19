4ef57e1a9aeefc882af197dc06b52ae7c2b2a43a
Michael Bragg <michael@michaelbragg.net>
2017-01-19 12:53:57 +0000

    Add WooCommerce Add-ons plugin base

---
+++ html/app/plugins-mu/tm-woocommerce.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:				WooCommerce Add-ons
+ * Plugin URI:        https://www.github.comtrinitymirror/creative-store/
+ * Version:						1.0.0
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.Text
+ * Text Domain:       tm-woocommerce
+ * Domain Path:       /languages
+ * Author:						Trinity Mirror
+ * Author URI:				http://www.trinitymirror.com
+ *
+ * @category     WordPress
+ * @package      tm-accounts
+ * @author       Trinity Mirror
+ * @license      GPL-2.0+
+ * @link         https://www.github.com/trinitymirror/creative-store/
+ */
